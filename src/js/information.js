2769ce2504c6c50ec690776aea859ef3d49a66d4
umi <0123mark3210@gmail.com>
2018-10-02 20:56:28 +0900

    fix fontawesome

--- src/js/information.js
+++ src/js/information.js
@@ -12,7 +12,7 @@ import './modules/custom-footer';
 import './modules/custom-drawer';
 
 
-import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/js/all.min';
 import '../scss/information.scss';
 
 main();
